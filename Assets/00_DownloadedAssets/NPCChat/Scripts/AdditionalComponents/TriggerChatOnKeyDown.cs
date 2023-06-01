66efd3d43a6b6fe471efdc692ebf74c4a3374824
EmmetOneT <emmetcarroll94@gmail.com>
2023-06-01 22:31:47 +0100

    NPC Talk
     Added an NPC with a capsule collider. Added an Idle animation. Added a panel with a Text component. Using my Intertact template, I created a text box system. When the player presses ‘E’ on the NPC a dialog box pops up with thext assigned to that NPC. Time stops and the players movement is disabled until the player presses ‘E’ again.

---
+++ Assets/00_DownloadedAssets/NPCChat/Scripts/AdditionalComponents/TriggerChatOnKeyDown.cs
@@ -0,0 +1,31 @@
+﻿namespace TurnTheGameOn.NPCChat
+{
+    using UnityEngine;
+
+    [RequireComponent(typeof(NPCChat))]
+    public class TriggerChatOnKeyDown : MonoBehaviour
+    {
+        public KeyCode keyCode;
+        private NPCChat npcChat;
+
+        void Start()
+        {
+            npcChat = GetComponent<NPCChat>();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(keyCode))
+            {
+                if (npcChat.talking)
+                {
+                    npcChat.FinishPage();
+                }
+                else
+                {
+                    npcChat.StartChat();
+                }
+            }
+        }
+    }
+}
